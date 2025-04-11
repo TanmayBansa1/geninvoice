846c306352627446aecb4f69a7d8090eca30fb93
tanmay bansal <121656091+TanmayBansa1@users.noreply.github.com>
2025-04-11 22:54:26 +0530

    added mark as paid functionality

--- src/components/invoice-actions.tsx
+++ src/components/invoice-actions.tsx
@@ -7,6 +7,7 @@ import { Button } from './ui/button'
 import { useRouter } from 'next/navigation';
 const InvoiceActions = ({isPaid, invoiceId}: {isPaid: boolean, invoiceId: string}) => {
   const deleteInvoice = api.invoice.deleteInvoice.useMutation();
+  const markAsPaid = api.invoice.markAsPaid.useMutation();
   const router = useRouter();
   return (
     <div>
@@ -14,30 +15,30 @@ const InvoiceActions = ({isPaid, invoiceId}: {isPaid: boolean, invoiceId: string
             <DropdownMenuTrigger className='text-right hover:cursor-pointer outline-none focus:ring-0 focus:outline-none'>
                 <MoreHorizontalIcon className='size-6 text-right'></MoreHorizontalIcon>
             </DropdownMenuTrigger>
-            <DropdownMenuContent align='end'>
+            <DropdownMenuContent align='end' className='gap-2 flex flex-col'>
                 <DropdownMenuLabel>Actions</DropdownMenuLabel>
                 <DropdownMenuItem asChild onClick={() => router.push(`/invoices/edit/${invoiceId}`)}>
-                    <Button  className='p-3'>
+                    <Button variant={'ghost'} className='p-3 w-full flex items-center justify-start text-left'>
                       <Pencil className="size-4" /> Edit Invoice
                     </Button>
                 </DropdownMenuItem>
                 <DropdownMenuItem asChild onClick={() => deleteInvoice.mutate({invoiceId})}>
-                    <Button variant={'link'} className='p-3'>
+                    <Button variant={'ghost'} className='p-3 w-full flex items-center justify-start text-left'>
                       <Trash className="size-4" /> Delete Invoice
                     </Button>
                 </DropdownMenuItem>
                 <DropdownMenuItem asChild>
-                    <Button variant={'link'} className='p-3' onClick={() => window.open(`/api/invoice/${invoiceId}`, '_blank')}>
+                    <Button variant={'ghost'} className='p-3 w-full flex items-center justify-start text-left' onClick={() => window.open(`/api/invoice/${invoiceId}`, '_blank')}>
                       <Download className="size-4" /> Download
                     </Button>
                 </DropdownMenuItem>
                 <DropdownMenuItem asChild>
-                    <Button variant={'link'} className='p-3' onClick={() => window.open(`/edit`, '_blank')}>
+                    <Button variant={'ghost'} className='p-3 w-full flex items-center justify-start text-left' onClick={() => window.open(`/edit`, '_blank')}>
                       <MailCheckIcon className="size-4" /> Send Reminder
                     </Button>
                 </DropdownMenuItem>
-                {!isPaid && <DropdownMenuItem asChild>
-                    <Button variant={'link'} className='p-3' onClick={() => window.open(`/edit`, '_blank')}>
+                {!isPaid && <DropdownMenuItem asChild onClick={() => markAsPaid.mutate({invoiceId})}>
+                    <Button variant={'ghost'} className='p-3 w-full flex items-center justify-start text-left'>
                       <CheckCircle2 className="size-4" /> Paid?
                     </Button>
                 </DropdownMenuItem>}
