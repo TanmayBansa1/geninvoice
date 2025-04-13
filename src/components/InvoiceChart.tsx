7bfb689a1cdb4d17710532984178da82643e2d71
tanmay bansal <121656091+TanmayBansa1@users.noreply.github.com>
2025-04-13 17:58:01 +0530

    added the chart component

---
+++ src/components/InvoiceChart.tsx
@@ -0,0 +1,40 @@
+'use client'
+import React from 'react'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card'
+import { ChartContainer, ChartTooltip, ChartTooltipContent } from './ui/chart'
+import { Line, LineChart, ResponsiveContainer, XAxis, YAxis } from 'recharts'
+
+type Props = {}
+
+const InvoiceChart = (props: Props) => {
+  return (
+    <Card className=''>
+        <CardHeader>
+            <CardTitle>Paid Invoices</CardTitle>
+            <CardDescription className='text-sm text-muted-foreground'>
+                You made $1000 in the last 30 days.
+            </CardDescription>
+        </CardHeader>
+        <CardContent>
+            <ChartContainer config={
+{                amount: {
+                    label: 'Amount',
+                    color: 'hsl(var(--primary))',
+                }}
+            } className='min-h-[300px]'>
+                <ResponsiveContainer width='100%' height='100%'>
+                    <LineChart>
+                        <XAxis dataKey="date"></XAxis>
+                        <YAxis ></YAxis>
+                        <ChartTooltip content={<ChartTooltipContent indicator='line'></ChartTooltipContent>}></ChartTooltip>
+                        <Line dataKey="amount" type='monotone' stroke='var(--color-amount)' strokeWidth={2}></Line>
+                    </LineChart>
+                </ResponsiveContainer>
+
+            </ChartContainer>
+        </CardContent>
+    </Card>
+  )
+}
+
+export default InvoiceChart
